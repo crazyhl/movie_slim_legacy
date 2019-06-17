b497ef4a6a19d067d0e59aec136544250fa355a8
M1racle <crazyhl@163.com>
2019-06-17 16:28:30 +0800

    命令支持 锁检查，保障同一时间只有一个任务在执行

--- src/Command/TestCommand.php
+++ src/Command/TestCommand.php
@@ -4,11 +4,14 @@
 namespace App\Command;
 
 
+use Symfony\Component\Console\Command\LockableTrait;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class TestCommand extends BaseCommand
 {
+    use LockableTrait;
+
     // the name of the command (the part after "bin/console")
     // 这个name 可以不用了，因为我们在config里面会配置name
     protected static $defaultName = 'test:command';
@@ -30,8 +33,20 @@ class TestCommand extends BaseCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        // 检查锁
+        if (!$this->lock()) {
+            $output->writeln('The command is already running in another process.');
+
+            return 0;
+        }
         // ...
-        $output->writeln('console 控制台输出');
+        $output->writeln('console 控制台输出1');
+        sleep(10);
+        $output->writeln('console 控制台输出2');
+
         $this->container->logger->info("来自控制台的日志");
+
+        // 释放锁
+        $this->release();
     }
 }
