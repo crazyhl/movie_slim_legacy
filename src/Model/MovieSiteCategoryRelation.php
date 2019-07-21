1f80ea6015632e79801f7edd9c5b97dffdebfe79
M1racle <crazyhl@163.com>
2019-07-21 23:13:20 +0800

    完成分类绑定

--- src/Model/MovieSiteCategoryRelation.php
+++ src/Model/MovieSiteCategoryRelation.php
@@ -3,6 +3,8 @@
 
 namespace App\Model;
 
+use Illuminate\Database\Eloquent\Builder;
+
 class MovieSiteCategoryRelation extends Base
 {
     public $timestamps = false;
@@ -11,5 +13,14 @@ class MovieSiteCategoryRelation extends Base
 
     protected $fillable = ['source_website_id', 'source_website_category_id', 'local_category_id', 'is_show'];
 
+    protected $primaryKey = ['source_website_id', 'source_website_category_id'];
     public $incrementing = false;
+
+    protected function setKeysForSaveQuery(Builder $query)
+    {
+        foreach($this->primaryKey as $pk) {
+            $query = $query->where($pk, $this->attributes[$pk]);
+        }
+        return $query;
+    }
 }
