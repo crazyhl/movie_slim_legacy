6c9ad278e87d171133dcc3860498ec5d550ecf9d
M1racle <crazyhl@163.com>
2019-07-27 23:01:49 +0800

    增加网址替换

--- src/Model/SourceMovie.php
+++ src/Model/SourceMovie.php
@@ -14,15 +14,42 @@ class SourceMovie extends Base
 
     public function getFormatMovieListAttribute($value)
     {
+        $replaceArr = [
+            'v-mtime.com' => 'youku.com-www-163.com',
+            'vip.okzybo.com' => 'youku.cdn-56.com',
+            'youku.com-www-163.com' => 'ifeng.com-v-ifeng.com',
+            'cdn.okokyun.com' => 'sohu.com-v-sohu.com',
+            'cn2.okokyun.com' => 'youku.cdn1-letv.com',
+            'yun.kakazy-yun.com' => 'zy.kubo-360-tudou.com',
+            'zy.kakazy-yun.com' => 'zy.baidu-360-yyy-kubo.com',
+            'kbzy.zxziyuan-yun.com' => 'youku.com-kubozy.com',
+        ];
+
         $movieList = explode('#', $this->movie_list);
         $formatMovieList = [];
         foreach ($movieList as $movie) {
             $movieInfo = explode('$', $movie);
+            foreach ($replaceArr as $from => $to) {
+                $url = str_replace($from, $to, $movieInfo[1]);
+            }
             $formatMovieList[] = [
                 'name' => $movieInfo[0],
-                'url' => $movieInfo[1],
+                'url' => $url,
             ];
         }
         return $formatMovieList;
+    }
+
+    public function getPicAttribute($pic)
+    {
+        $replaceArr = [
+            'pic.china-gif.com' => 'img.pic-imges.com'
+        ];
+
+        foreach ($replaceArr as $from => $to) {
+            $pic = str_replace($from, $to, $pic);
+        }
+
+        return $pic;
     }
 }
