c4c7036fb9fd366858ecc1cee77daf600140ddbc
M1racle <crazyhl@163.com>
2019-08-08 16:56:37 +0800

    测试优化 https

--- src/Model/Movie.php
+++ src/Model/Movie.php
@@ -16,4 +16,13 @@ class Movie extends Base
     {
         return $this->belongsTo(Category::class, 'category_id', 'id');
     }
+
+    public function getPicAttribute($pic)
+    {
+        if (substr($pic, 0, 5) === 'http:') {
+            $pic = substr($pic, 5);
+        }
+
+        return $pic;
+    }
 }
